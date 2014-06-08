fcc2501aa0584049e500c93f38ddd87d4f7b38d6
Sebastian M. Perez <perezsebastianm@gmail.com>
2014-06-07 22:59:35 -0300

    Restruccutacion

---
+++ admin/global_constants.php
@@ -0,0 +1,13 @@
+<?php
+//EDITABLE
+define('SERVER_ROOT', 'C:/xampp/htdocs/');
+define('ROOT_FOLDER_NAME', 'ayudaresfacil');
+
+//APPLICATION CONSTANTS
+//SI EDITAS ES BAJO TU PROPIO RIESGO
+define('BASE_PATH', SERVER_ROOT. ROOT_FOLDER_NAME .'/admin');
+define('SOURCE_PATH', '/'.ROOT_FOLDER_NAME.'/source/');
+define('APP_NAME','Proyecto UNLAM');	//NOMBRE DE LA APLICACION
+define('APP_DEVELOPER','spor3');	//EMPRESA DESSARROLLADORA
+define('APP_COMPANY','UNLAM');	//CLIENTE
+define('APP_TITLE','Proyecto UNLAM :: Sistema de Administración');	//TITULO DE LA APLICACION
