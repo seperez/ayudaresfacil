21f5ded0fce98d113d6b2baa43c9ff68a71591cb
Sebastian Perez <sperez@hexacta.com>
2014-06-09 09:50:03 -0300

    Reordeno atributos en clase Publication

--- admin/application/libraries/Publication.php
+++ admin/application/libraries/Publication.php
@@ -2,36 +2,36 @@
 
 class CI_Publication {
 	private $id;
-	private $creationDate;
 	private $title;
 	private $description;
-	private $expirationDate;
 	private $category;
 	private $subcategory;
-	private $views;
-	private $processState;
 	private $object;
 	private $quantity;
+	private $views;
+	private $processState;
+	private $creationDate;
+	private $expirationDate;
 
 	public function getId() {return $this->id;}
-
-	public function getCreationDate(){return $this->creationDate;}
-	public function setCreationDate($creationDate){$this->creationDate = $creationDate;}
 	
 	public function getTitle(){return $this->title;}
 	public function setTitle($title){$this->title = $title;}
 	
 	public function getDescription(){return $this->description;}
 	public function setDescription($description){$this->description = $description;}
-	
-	public function getExpirationDate(){return $this->expirationDate;}
-	public function setExpirationDate($expirationDate){$this->expirationDate = $expirationDate;}
 
 	public function getCategory(){return $this->category;}
 	public function setCategory($category){$this->category = CI_Category::getById($category);}
 
 	public function getSubcategory(){return $this->subcategory;}
 	public function setSubcategory($subcategory){$this->subcategory = CI_Subcategory::getById($subcategory);}
+	
+	public function getObject(){return $this->object;}
+	public function setObject($object){$this->object = CI_Object::getById($object);}
+
+	public function getQuantity(){return $this->quantity;}
+	public function setQuantity($quantity){$this->quantity = $quantity;}
 
 	public function getViews(){return $this->views;}
 	public function setViews($views){$this->views = $views;}
@@ -39,12 +39,11 @@ class CI_Publication {
 	public function getProcessState(){return $this->processState;}
 	public function setProcessState($processState){$this->processState = CI_ProcessState::getById($processState);}
 
-	public function getObject(){return $this->object;}
-	public function setObject($object){$this->object = CI_Object::getById($object);}
-
-	public function getQuantity(){return $this->quantity;}
-	public function setQuantity($quantity){$this->quantity = $quantity;}
-
+	public function getCreationDate(){return $this->creationDate;}
+	public function setCreationDate($creationDate){$this->creationDate = $creationDate;}
+	
+	public function getExpirationDate(){return $this->expirationDate;}
+	public function setExpirationDate($expirationDate){$this->expirationDate = $expirationDate;}
 	/**
 	 * Devuelve la informacion cargada del objeto 
 	 * 		
@@ -56,16 +55,16 @@ class CI_Publication {
 	protected function getData(){
 		$object = new stdClass();
 		$object->id = $this->publicationId;
-		$object->creationDate = $this->creationDate;
 		$object->title = $this->title;
 		$object->description = $this->description;
-		$object->expirationDate = $this->expirationDate;
 		$object->category = $this->category;
 		$object->subcategory = $this->subcategory;
-		$object->views = $this->views;
-		$object->processState = $this->processState;
 		$object->object = $this->object;
 		$object->quantity = $this->quantity;
+		$object->views = $this->views;
+		$object->processState = $this->processState;
+		$object->creationDate = $this->creationDate;
+		$object->expirationDate = $this->expirationDate;
 		return $object;
 	}
 
@@ -75,16 +74,16 @@ class CI_Publication {
 		}	
 		$publication = new self;
 		$publication->id = (isset($row->publication_id)) ? $row->publication_id : 0;
-		$publication->creationDate = (isset($row->creation_date)) ? $row->creation_date : '';
 		$publication->title = (isset($row->title)) ? $row->title : '';
 		$publication->description = (isset($row->description)) ? $row->description : '';
-		$publication->expirationDate = (isset($row->expiration_date)) ? $row->expiration_date : '';
 		$publication->category = (isset($row->category_id)) ? CI_Category::getById($row->category_id) : '';
 		$publication->subcategory = (isset($row->subcategory_id)) ? CI_Subcategory::getById($row->subcategory_id) : '';
-		$publication->views = (isset($row->views)) ? $row->views : '';
-		$publication->processState = (isset($row->process_state_id)) ? CI_ProcessState::getById($row->process_state_id) : '';
 		$publication->object = (isset($row->object_id)) ? CI_Object::getById($row->object_id) : '';
 		$publication->quantity = (isset($row->quantity)) ? $row->quantity : '';
+		$publication->views = (isset($row->views)) ? $row->views : '';
+		$publication->processState = (isset($row->process_state_id)) ? CI_ProcessState::getById($row->process_state_id) : '';
+		$publication->creationDate = (isset($row->creation_date)) ? $row->creation_date : '';
+		$publication->expirationDate = (isset($row->expiration_date)) ? $row->expiration_date : '';
 		return $publication;
 	}
 
