15cc770c205e6f3217ec990eaeeb926c90ff69fc
Sebastian Perez <sperez@hexacta.com>
2014-07-08 16:26:04 -0300

    Actualizo vista de registracion, traduzco a español

--- client/src/assets/js/app-template.js
+++ client/src/assets/js/app-template.js
@@ -652,23 +652,23 @@ $(document).ready(function(){
 // 			}); 
 // 	}
 	
-// 	if ($('.inline-popups').length > 0){
-// 			$('.inline-popups').magnificPopup({
-// 			  delegate: 'a',
-// 			  removalDelay: 500,
-// 			  callbacks: {
-// 				beforeOpen: function() {
-// 				   this.st.mainClass = this.st.el.attr('data-effect');
-// 				}
-// 			  },
-// 			  midClick: true
-// 			});
-// 	}
-// 	$('.magnific-img').magnificPopup({
-// 		type:'image',
-// 		removalDelay: 300,
-// 		mainClass: 'mfp-fade'
-// 	});
+	if ($('.inline-popups').length > 0){
+			$('.inline-popups').magnificPopup({
+			  delegate: 'a',
+			  removalDelay: 500,
+			  callbacks: {
+				beforeOpen: function() {
+				   this.st.mainClass = this.st.el.attr('data-effect');
+				}
+			  },
+			  midClick: true
+			});
+	}
+	$('.magnific-img').magnificPopup({
+		type:'image',
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
+	});
 // 	/** END MAGNIFIC POPUP **/
 	
 
