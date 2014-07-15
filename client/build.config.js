51bfa3c9a563cca28b3078a82f14e5c4c8ec924c
Sebastian Perez <sperez@hexacta.com>
2014-07-15 11:09:15 -0300

    linkeo libreria magnific-popup

--- client/build.config.js
+++ client/build.config.js
@@ -67,7 +67,8 @@ module.exports = {
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/magnific-popup/dist/jquery.magnific-popup.min.js'
     ],
     css: [
       'vendor/bootstrap/dist/css/bootstrap.min.css',
