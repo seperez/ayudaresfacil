427b308a62e59d7d82969402614c81a474258ce7
Sabrina Casado <sabrinamarielcasado@gmail.com>
2014-09-20 15:49:22 -0300

    Agrego al modulo ofrecimientos: Ver detalle

--- client/src/app/offer/offer.js
+++ client/src/app/offer/offer.js
@@ -15,6 +15,12 @@ angular.module( 'AyudarEsFacilApp.offer', [
         templateUrl: 'offer/offer-create.tpl.html',
         data:{ pageTitle: 'Crear Ofrecimiento' }
     });
+    $stateProvider.state( 'web.offerDetail', {     
+        url: '/ofrecimiento-detalle',
+        controller: 'OfferDetailCtrl',
+        templateUrl: 'offer/offer-detail.tpl.html',
+        data:{ pageTitle: 'Detalle del Ofrecimiento' }
+    });
 })
 
 .controller( 'OfferCtrl', function OfferCtrl( $scope ) {
